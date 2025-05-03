51ff2e594b3291a89645a744dca0ff09031b5ebd
Jiwoo Park <jiwoopark727@gmail.com>
2025-05-03 19:23:05 +0900

    server side fetch와 loading components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,29 +1,19 @@
-'use client';
-
 import { Metadata } from 'next';
-import { useEffect, useState } from 'react';
-import { json } from 'stream/consumers';
 
-// export const metadata: Metadata = {
-//   title: 'Home',
-// };
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
-export default function Tomato() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      'https://nomad-movies.nomadcoders.workers.dev/movies'
-    );
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
 
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>;
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
