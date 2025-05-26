47e9ed587e0741089624ebbec46d8423c95cbd39
Jiwoo Park <jiwoopark727@gmail.com>
2025-05-26 21:00:37 +0900

    fix: params 타입 지정 오류 해결 - 인자에서 직접 구조 분해 x

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -6,21 +6,21 @@ interface IParams {
   params: { id: string };
 }
 
-export async function generateMetadata({ params: { id } }: IParams) {
-  const movie = await getMovie(id);
+export async function generateMetadata({ params }: IParams) {
+  const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+export default async function MovieDetailPage({ params }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
-        <MovieInfo id={id}></MovieInfo>
+        <MovieInfo id={params.id}></MovieInfo>
       </Suspense>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
-        <MovieVideos id={id}></MovieVideos>
+        <MovieVideos id={params.id}></MovieVideos>
       </Suspense>
     </div>
   );
