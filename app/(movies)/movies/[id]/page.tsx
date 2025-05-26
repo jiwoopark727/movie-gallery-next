fc9e28c4e40d08ec9eaddebbb43829e2a3b1c2f7
Jiwoo Park <jiwoopark727@gmail.com>
2025-05-26 21:12:01 +0900

    fix: 내부 타입 호환성 문제 - PageProps

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,25 +2,25 @@ import { Suspense } from 'react';
 import MovieInfo, { getMovie } from '../../../../components/movie-info';
 import MovieVideos from '../../../../components/movie-videos';
 
-interface IParams {
+type PageProps = {
   params: { id: string };
-}
+};
 
-export async function generateMetadata({ params }: IParams) {
+export async function generateMetadata({ params }: PageProps) {
   const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params }: IParams) {
+export default async function MovieDetailPage({ params }: PageProps) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
-        <MovieInfo id={params.id}></MovieInfo>
+        <MovieInfo id={params.id} />
       </Suspense>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
-        <MovieVideos id={params.id}></MovieVideos>
+        <MovieVideos id={params.id} />
       </Suspense>
     </div>
   );
