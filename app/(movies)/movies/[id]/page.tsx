620cf4ba25470fd43faea05461412c6b25acda32
Jiwoo Park <jiwoopark727@gmail.com>
2025-05-26 21:26:24 +0900

    fix: 인라인 타입 선언으로 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,18 +2,18 @@ import { Suspense } from 'react';
 import MovieInfo, { getMovie } from '../../../../components/movie-info';
 import MovieVideos from '../../../../components/movie-videos';
 
-type PageProps = {
-  params: { id: string };
-};
-
-export async function generateMetadata({ params }: PageProps) {
+export async function generateMetadata({ params }: { params: { id: string } }) {
   const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params }: PageProps) {
+export default async function MovieDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
